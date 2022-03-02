fc15999f7c143935970628c4aa71ee1be1eaad19
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-03-02 11:25:09 +0000

    Ask for user input

---
+++ Tasks.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Tasks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter task: ");
+
+        String task = scanner.nextLine();
+        System.out.println("Task is: " + task);
+    }
+}
