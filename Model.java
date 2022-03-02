2bc634874d324e8e41b7eb239e37c6f46f04244a
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-03-02 15:37:27 +0000

    Add task to dictionary in correct category

--- Model.java
+++ Model.java
@@ -1,12 +1,29 @@
+import java.util.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Model {
 
-    ArrayList<String> tasksList = new ArrayList<String>();
+    //ArrayList<String> tasksList = new ArrayList<String>();
+    HashMap<String, ArrayList<String>> allTasks = new HashMap<String, ArrayList<String>>();
+    ArrayList<String> foodTasks = new ArrayList<String>();
+    ArrayList<String> clothesTasks = new ArrayList<String>();
+    ArrayList<String> schoolTasks = new ArrayList<String>();
+    ArrayList<String> houseTasks = new ArrayList<String>();
+
+    public Model() {
+        allTasks.put("food", foodTasks);
+        allTasks.put("clothes", clothesTasks);
+        allTasks.put("school", schoolTasks);
+        allTasks.put("house", houseTasks);
+    }
 
     public void storeTask(String s){
-        tasksList.add(s);
+        if (s.contains("eat")) {
+            foodTasks.add(s);
+            allTasks.replace("food", foodTasks);
+        }
 
-        System.out.println(tasksList.size());
+        System.out.println(allTasks.get("food"));
     }
 }
