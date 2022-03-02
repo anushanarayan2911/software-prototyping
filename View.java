103edc7797cd95f50074807d75e2038d60821e1b
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-03-02 13:35:49 +0000

    Change layout of GUI

--- View.java
+++ View.java
@@ -7,16 +7,25 @@ public class View {
     private JFrame frame;
     private JPanel panel1;
     private JLabel label;
+    private JLabel label2;
 
     public View() {
         frame = new JFrame();
         panel1 = new JPanel();
         label = new JLabel("hi");
+        label2 = new JLabel("bye");
 
         frame.setTitle("Tasks");
         frame.setSize(500, 500);
+        frame.setLayout(null);
+        
+        panel1.setSize(100, 500);
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
+        panel1.setLocation(50, 50);
 
-        frame.add(label);
+        panel1.add(label);
+        panel1.add(label2);
+        frame.add(panel1);
     }
 
     public void displayScreen() {
