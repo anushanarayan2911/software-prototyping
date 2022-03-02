5660fc87dedd046721787156206551f75b2f318e
anushanarayan2911 <anusha.narayan2002@outlook.com>
2022-03-02 14:31:04 +0000

    Add text field to enter task

--- View.java
+++ View.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 
 
@@ -7,21 +9,31 @@ public class View {
     private JFrame frame;
     private JPanel panel1;
     private JLabel enterTaskLabel;
+    private JTextField enterTaskTextField;
 
     public View() {
         frame = new JFrame();
         panel1 = new JPanel();
         enterTaskLabel = new JLabel("Enter task");
+        enterTaskTextField = new JTextField();
 
         frame.setTitle("Tasks");
         frame.setSize(500, 500);
         frame.setLayout(null);
         
-        panel1.setSize(500, 500);
-        panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
-        panel1.setLocation(210, 100);
+        panel1.setSize(100, 50);
+        BoxLayout boxLayout = new BoxLayout(panel1, BoxLayout.Y_AXIS);
+        panel1.setLayout(boxLayout);
+        panel1.setLocation(210, 210);
+
+        enterTaskLabel.setLocation(100, 0);
+
+        Dimension size = new Dimension(100, 10);
+        enterTaskTextField.setSize(size);
+        enterTaskTextField.setBorder(null);
 
         panel1.add(enterTaskLabel);
+        panel1.add(enterTaskTextField);
 
         frame.add(panel1);
     }
